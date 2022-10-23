9722c69d4725734ce1eff8b83438ac99e3312671
tidus <tidussss@yandex.ru>
2022-10-23 15:40:02 +0300

    change

--- project/services/users_service.py
+++ project/services/users_service.py
@@ -1,11 +1,17 @@
+from sqlalchemy.orm import scoped_session
+
 from project.dao import UserDAO
 from project.exceptions import ItemNotFound
 from project.schemas.user import UserSchema
 from project.services.base import BaseService
-from project.tools.security import generate_password_digest
+from project.tools.security import generate_password_digest, get_password_hash
 
 
 class UsersService(BaseService):
+    def __init__(self, session: scoped_session):
+        super().__init__(session)
+        self.dao = None
+
     def get_item_by_id(self, pk: int):
         user = UserDAO(self._db_session).get_by_id(pk)
         if not user:
@@ -34,9 +40,31 @@ class UsersService(BaseService):
         return UserSchema().dump(user)
 
 
-    def update_password(self, new_pd):
-        user_password_1 = new_pd.get("password_1")
-        user_password_2 = new_pd.get("password_2")
-        return UserSchema().dump(user_password_1 or user_password_2)
+    # def update_password(self, new_pd):
+    #     user_password_1 = new_pd.get("password_1")
+    #     user_password_2 = new_pd.get("password_2")
+    #     return UserSchema().dump(user_password_1 or user_password_2)
 
+    def update_password(self, email, old_password, new_password):
+        user = self.dao.UserDAO(self._db_session).get_by_email(email)
+        password_hash = user.password
 
+        if get_password_hash(password_hash=password_hash, password=old_password):
+            new_password_hash = get_password_hash(new_password)
+            user.password = new_password_hash
+            self.dao.UserDAO(self._db_session).update(user)
+            return UserSchema().dump(user)
+        else:
+            return None
+
+    # def update_password(self, email, old_password, new_password):
+    #     user = self.dao.get_by_email(email)
+    #     password_hash = user.password
+    #
+    #     if compose_passwords(password_hash=password_hash, password=old_password):
+    #         new_password_hash = generate_password_hash(new_password)
+    #         user.password = new_password_hash
+    #         self.dao.update(user)
+    #         return self.dao
+    #     else:
+    #         return None
