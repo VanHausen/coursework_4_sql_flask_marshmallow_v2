7708e573cb039265a2f9bb928ae0aa2f01add0a6
tidus <tidussss@yandex.ru>
2022-10-30 17:56:43 +0300

    change

--- project/schemas/user.py
+++ project/schemas/user.py
@@ -5,8 +5,8 @@ from load_fixtures import genre
 
 class UserSchema(Schema):
     id = fields.Int(required=True, example=1)
-    email = fields.Str(required=True, max_length=100, example='fields@gmail.com')
-    password = fields.Str(required=True, max_length=100, example='1f2344vv23cv')
-    name = fields.Str(required=True, max_length=100, example='Evgen')
+    email = fields.Str(required=True, example='fields@gmail.com')
+    password = fields.Str(required=True, max_length=100,  example='1f2344vvcv')
+    name = fields.Str(required=True, max_length=100,  example='Evgen')
     surname = fields.Str(required=True, max_length=100, example='Komarov')
-    favorite_genre = fields.Nested(genre)
+    favorite_genre = fields.Str(required=True, max_length=100)
