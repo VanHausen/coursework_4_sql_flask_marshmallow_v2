50c8bce30add1c80caa5bf93057b8c90b98f94b9
tidus <tidussss@yandex.ru>
2022-10-30 17:57:34 +0300

    change

--- project/schemas/user.py
+++ project/schemas/user.py
@@ -1,12 +1,10 @@
 from marshmallow import fields, Schema
 
-from load_fixtures import genre
-
 
 class UserSchema(Schema):
     id = fields.Int(required=True, example=1)
     email = fields.Str(required=True, example='fields@gmail.com')
-    password = fields.Str(required=True, max_length=100,  example='1f2344vvcv')
-    name = fields.Str(required=True, max_length=100,  example='Evgen')
-    surname = fields.Str(required=True, max_length=100, example='Komarov')
-    favorite_genre = fields.Str(required=True, max_length=100)
+    password = fields.Str(required=True, example='1f2344vvcv')
+    name = fields.Str()
+    surname = fields.Str()
+    favorite_genre = fields.Str()
